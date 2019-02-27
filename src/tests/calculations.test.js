0dcc4dc1b357f0dc8a0338ae52e1216b799f40b0
unknown <taylorwcolten@gmail.com>
2019-02-26 20:26:27 -0600

    subtract test

--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -5,4 +5,9 @@ describe("add", () => {
     expect(add(1, 2)).toBe(3);
   });
 });
-d
+
+describe("subtract", () => {
+  it("should subtract 4 from 5 and return 1", () => {
+    expect(subtract(5, 4)).toBe(1);
+  });
+});
